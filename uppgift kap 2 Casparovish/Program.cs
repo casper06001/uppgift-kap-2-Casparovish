7e135380b1173c49fc852c169d88017f270b6113
Pingvu <nhlpool27@gmail.com>
2023-09-28 12:57:09 +0200

    Add project files.

---
+++ uppgift kap 2 Casparovish/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace uppgift_kapitel_2_casparovish
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            string x = Console.ReadLine();
+            string favoritbok = x;
+            Console.WriteLine(favoritbok + " är en fantastisk bok!");
+            Console.ReadKey();
+        }
+    }
+}
